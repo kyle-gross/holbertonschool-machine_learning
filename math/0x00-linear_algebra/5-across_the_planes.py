ddb29465a8e7f94ad1afc2b17f70ebb4a139c0f5
kyle-gross <kyg555@outlook.com>
2021-07-15 20:03:34 +0000

    Change mat2 to mat1

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -18,7 +18,7 @@ def add_matrices2D(mat1, mat2):
         return None
 
     return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
-            for i in range(len(mat2))]
+            for i in range(len(mat1))]
 
 
 def matrix_shape(matrix):
