add4ff52e99e7fead9b909202ada787468c11d30
kyle-gross <kyg555@outlook.com>
2021-07-15 19:41:43 +0000

    Fix pycodestyle

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -3,8 +3,12 @@
 Contains the function 'matrix_transpose' which returns the transpose
 of a 2d matrix
 """
+
+
 def matrix_transpose(matrix):
     """
     Returns the transpose of a 2d matrix
     """
-    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    x = matrix
+
+    return [[x[j][i] for j in range(len(x))] for i in range(len(x[0]))]
