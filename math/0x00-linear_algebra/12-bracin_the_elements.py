01b3a1beb5255055868a42e1c366c0add9f30f89
kyle-gross <kyg555@outlook.com>
2021-07-15 23:05:18 +0000

    Fix docstring for pycode

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -3,7 +3,8 @@
 Contains the function 'np_elementwise' which performs element-wise
 addition, subtraction, multiplication, and division.
     * You can assume that mat1 and mat2 can be interpreted as numpy.ndarrays
-    * You should return a tuple containing the element-wise sum, difference, product, and quotient, respectively
+    * You should return a tuple containing the element-wise
+      sum, difference, product, and quotient, respectively
     * You are not allowed to use any loops or conditional statements
     * You can assume that mat1 and mat2 are never empty
 """
