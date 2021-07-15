0b298cec4cd2aa70bffb037e532aff301c2e3653
kyle-gross <kyg555@outlook.com>
2021-07-15 19:20:18 +0000

    Update for pycodestyle

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -4,10 +4,12 @@ Contains the function 'matrix_shape' which calculates the shape
 of a matrix.
 """
 
+
 def matrix_shape(matrix):
     """
     Calculates the shape of a matrix.
-    * You can assume all elements in the same dimension are of the same type/shape
+    * You can assume all elements in the same dimension are of the same
+      type/shape
     * The shape should be returned as a list of integers
     """
 
