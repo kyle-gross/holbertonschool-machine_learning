4ab5a078e45fadfefb43a89154b209ce8236bce9
kyle-gross <kyg555@outlook.com>
2021-07-15 22:00:53 +0000

    Add task 8

---
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Contains the function 'mat_mul' which performs matrix multiplication
+"""
+
+
+def mat_mul(mat1, mat2):
+    """
+    Performs matrix multiplication
+    * You can assume that mat1 and mat2 are 2D matrices containing ints/floats
+    * You can assume all elements in the same dimension are of the same
+      type/shape
+    * You must return a new matrix
+    * If the two matrices cannot be multiplied, return None
+    """
+    if len(mat1[0]) != len(mat2):
+        return None
+
+    return [[sum(a * b for a, b in zip(mat1_row, mat2_col))
+            for mat2_col in zip(*mat2)]
+            for mat1_row in mat1]
