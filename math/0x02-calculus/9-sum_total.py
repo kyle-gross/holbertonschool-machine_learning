3a6c9ab3b486318796aaec1e66ee388bcf22a9f7
kyle-gross <kyg555@outlook.com>
2021-07-23 00:12:23 +0000

    Remove recursion, substitute for python sum of series formula

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This module contains the function 'summaation_i_squared' which
+This module contains the function 'summation_i_squared' which
 calculates sum_{i=1}^{n} i^2
     * <n> is the stopping condition
     * Return the integer value of the sum
@@ -17,8 +17,7 @@ def summation_i_squared(n):
         # Return None if <n> is not a valid number
         return None
 
-    if n == 1:
-        # Base case - stop when n reaches 1
-        return n ** 2 
-
-    return summation_i_squared(n - 1) + n ** 2
+    # Mathematical formula for Python sum of series
+    # 1^2 + 2^2 + 3^2...
+    # = (n(n+1)(2n+1)) / 6
+    return int((n * (n + 1) * (2 * n + 1)) / 6)
