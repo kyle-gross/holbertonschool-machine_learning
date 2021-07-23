f239215c0e0b4b4c5f0e57522afac15bee46297f
kyle-gross <kyg555@outlook.com>
2021-07-23 03:35:06 +0000

    Fix [0] check. From 'is' to '=='

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -23,12 +23,9 @@ def poly_integral(poly, C=0):
     if type(poly) is not list or type(C) is not int:
         return None
 
-    if len(poly) == 0:
-        return None
-
     integrals = [C]
 
-    if poly is [0]:
+    if poly == [0]:
         return integrals
 
     def int_check(x): return int(x) if x.is_integer() else x
