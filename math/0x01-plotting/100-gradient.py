d845f363d8c6404e5fd84cf8efd20d76e19f76b5
kyle-gross <kyg555@outlook.com>
2021-07-23 00:21:26 +0000

    Incoroporate type check, and 0 derivative

---
+++ math/0x01-plotting/100-gradient.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+
+x = np.random.randn(2000) * 10
+y = np.random.randn(2000) * 10
+z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+
+# Plot settings
+plt.title('Mountain Elevation')
+plt.xlabel('x coordinate (m)')
+plt.ylabel('y coordinate (m)')
+
