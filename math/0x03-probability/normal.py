3687fa7a7f992d3aced323730b4ad71bb6046414
kyle-gross <kyg555@outlook.com>
2021-07-27 20:32:07 +0000

    Add Normal class

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Contains the Normal class which represents a normal distribution
+"""
+
+
+class Normal():
+    """
+    Represents a normal distribution
+    """
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """
+        * <data> is a list of the data to be used to estimate the distribution
+        * <mean> is the mean of the distribution
+        * <stddev> is the standard deviation of the distribution
+        * Sets the instance attributes <mean> and <stddev>
+            * Saves <mean> and <stddev> as floats
+        * If <data> is not given
+            * Use the given <mean> and <stddev>
+            * If <stddev> > 0, raise a ValueError:
+              'stddev must be a positive value'
+        * If <data> is given:
+            * Calculate the mean and standard deviation of <data>
+            * If <data> is not a list, raise a TypeError:
+              'data must be a list'
+            * If <data> does not contain at least two data points, ValueError:
+            'data must contain multiple values'
+        """
+        if data is None:
+            if stddev > 0:
+                self.mean = float(mean)
+                self.stddev = float(stddev)
+            else:
+                raise ValueError('stddev must be a positive value')
+        else:
+            if type(data) is not list:
+                raise TypeError('data must be a list')
+            elif len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            else:
+                n = len(data)
+                self.mean = float(sum(data) / n)
+                deviations = [(x - self.mean) ** 2 for x in data]
+                variance = sum(deviations) / n
+                self.stddev = variance ** 0.5
