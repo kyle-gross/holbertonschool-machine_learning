7ab4c391183669b17f6210e7c0e67c799cfc736c
kyle-gross <kyg555@outlook.com>
2021-08-06 00:20:30 +0000

    Add some more files

---
+++ supervised_learning/0x01-classification/17-deep_neural_network.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Contains the class DeepNeuralNetwork.
+"""
+import numpy as np
+
+
+class DeepNeuralNetwork():
+    """
+    Defines a deep neural network performing binary classification.
+    """
+    def __init__(self, nx, layers):
+        """
+        * @nx: number of input features
+            * Must be int. Must be > 0.
+        * @layers: list representing # of nodes in each layer of network.
+            * Must be list. Must NOT be empty.
+            * 1st value represents the number of nodes in the first layer.
+            * Elements must ALL be positive integers.
+        Sets the public instance attributes:
+            * L: # of layers in neural network.
+            * cache: dict to hold all intermediary values of network.
+            * weights: dict to hold weights and biases of network.
+        """
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        elif nx < 1:
+            raise ValueError('nx must be a positive integer')
+        elif type(layers) is not list or len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
+        elif min(layers) < 1:
+            raise TypeError('layers must be a list of positive integers')
+        else:
+            self.__L = len(layers)
+            self.__cache = {}
+            self.__weights = {}
+            prev = nx
+            for i in range(len(layers)):
+                w = np.random.randn(layers[i], prev) * np.sqrt(2/prev)
+                prev = layers[i]
+                self.__weights['W{}'.format(i + 1)] = w
+                dim = len(self.__weights['W{}'.format(i + 1)])
+                self.__weights['b{}'.format(i + 1)] = np.zeros((dim, 1))
+
+    @property
+    def L(self):
+        """L getter"""
+        return self.__L
+
+    @property
+    def cache(self):
+        """cache getter"""
+        return self.__cache
+
+    @property
+    def weights(self):
+        """weights getter"""
+        return self.__weights
