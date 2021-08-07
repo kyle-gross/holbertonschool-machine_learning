620aa3adedfaf3f225902cf1d81f52bfed74b2ab
kyle-gross <kyg555@outlook.com>
2021-08-07 19:17:06 +0000

    Fix typo

--- supervised_learning/0x01-classification/14-neural_network.py
+++ supervised_learning/0x01-classification/14-neural_network.py
@@ -146,7 +146,7 @@ class NeuralNetwork():
         if iterations < 1:
             raise ValueError('iterations must be a positive integer')
         if type(alpha) is not float:
-            raise TypeError('aplha must be a float')
+            raise TypeError('alpha must be a float')
         if alpha <= 0:
             raise ValueError('alpha must be positive')
         for _ in range(0, iterations):
