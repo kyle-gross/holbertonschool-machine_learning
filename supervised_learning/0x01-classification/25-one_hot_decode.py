0a33279c3cdae0d858e4789454eca85af1d814d5
kyle-gross <kyg555@outlook.com>
2021-08-09 22:03:41 +0000

    Add ndim check

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -16,4 +16,6 @@ def one_hot_decode(one_hot):
     """
     if type(one_hot) is not np.ndarray:
         return None
+    if one_hot.ndim != 2:
+        return None
     return np.argmax(one_hot, axis=0)
