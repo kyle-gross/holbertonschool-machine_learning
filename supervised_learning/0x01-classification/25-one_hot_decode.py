f148208a55c17f93e80db2a5e3cd13b0935b4e5b
kyle-gross <kyg555@outlook.com>
2021-08-09 22:00:43 +0000

    Add 25

---
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Contains the function one_hot_decode().
+"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """
+    Converts a one-hot matrix into a vector of labels.
+    * @one_hot: one-hot encoded numpy.ndarray. Shape: (classes, m)
+        * classes: max # of classes
+        * m: number of examples
+    Return: numpy.ndarray. Shape: (m,). Contains numeric labels for
+            each example. Or None if failure
+    """
+    if type(one_hot) is not np.ndarray:
+        return None
+    return np.argmax(one_hot, axis=0)
