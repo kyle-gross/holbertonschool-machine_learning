8935f1dc644b92bf474a6e06b7d5d83be193243f
Kyle <kyg555@outlook.com>
2021-08-15 23:49:55 -0700

    Fix prediction step

--- supervised_learning/0x00-tensorflow/2-forward_prop.py
+++ supervised_learning/0x00-tensorflow/2-forward_prop.py
@@ -8,16 +8,16 @@ create_layer = __import__('1-create_layer').create_layer
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     """Creates the forward propagation graph for the neural network.
-    
+
     Args:
       x: placeholder for input data
       layer_sizes: list containing the number of nodes in each layer
       activations: list containing activation functions for each layer
-    
+
     Returns:
         prediction of network in tensor form
     """
     prediction = x
     for i in range(len(layer_sizes)):
-        prediction = create_layer(x, layer_sizes[i], activations[i])
+        prediction = create_layer(prediction, layer_sizes[i], activations[i])
     return prediction
