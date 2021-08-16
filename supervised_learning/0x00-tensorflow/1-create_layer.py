e66fda614f4d5e5fd602d100975eb2d7caf4e6d4
Kyle <kyg555@outlook.com>
2021-08-15 23:48:38 -0700

    Fix task 2

--- supervised_learning/0x00-tensorflow/1-create_layer.py
+++ supervised_learning/0x00-tensorflow/1-create_layer.py
@@ -6,12 +6,15 @@ import tensorflow as tf
 
 
 def create_layer(prev, n, activation):
-    """
-    Creates a layer of neurons
-    * @prev: tensor output of the previous layer
-    * @n: number of nodes in the layer to create
-    * @activation: activation function that the layer should use
-    Return: tensor output of the layer
+    """Creates a layer of neurons
+
+    Args:
+      prev: tensor output of the previous layer
+      n: number of nodes in the layer to create
+      activation: activation function that the layer should use
+    
+    Returns:
+      tensor output of the layer
     """
     theta = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     return tf.layers.dense(prev, n, activation, kernel_initializer=theta)
--- supervised_learning/0x00-tensorflow/2-forward_prop.py
+++ supervised_learning/0x00-tensorflow/2-forward_prop.py
@@ -7,13 +7,17 @@ create_layer = __import__('1-create_layer').create_layer
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
+    """Creates the forward propagation graph for the neural network.
+    
+    Args:
+      x: placeholder for input data
+      layer_sizes: list containing the number of nodes in each layer
+      activations: list containing activation functions for each layer
+    
+    Returns:
+        prediction of network in tensor form
     """
-    Creates the forward propagation graph for the neural network.
-    * @x: placeholder for input data
-    * @layer_sizes: list containing the number of nodes in each layer
-    * @activations: list containing activation functions for each layer
-    Return: prediction of network in tensor form
-    """
+    prediction = x
     for i in range(len(layer_sizes)):
         prediction = create_layer(x, layer_sizes[i], activations[i])
     return prediction
