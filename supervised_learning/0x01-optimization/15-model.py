f658816ceadb6e788565840b95179c0c1903481f
Kyle <kyg555@outlook.com>
2021-08-23 12:57:02 -0700

    Use global_step correctly and also correct number of decay_steps (=== total batches).

--- supervised_learning/0x01-optimization/15-model.py
+++ supervised_learning/0x01-optimization/15-model.py
@@ -5,103 +5,25 @@ import numpy as np
 import tensorflow as tf
 
 
-def calculate_accuracy(y, y_pred):
-    """Determines accuracy of neural network
-    Args:
-      y: placeholder for labels of input data
-      y_pred: tensor containing network's predictions
-    Returns:
-      tensor containing the decimal accuracy of the prediction
-    """
-    y = tf.argmax(y, axis=1)
-    y_pred = tf.argmax(y_pred, axis=1)
-    return tf.reduce_mean(tf.cast(tf.equal(y, y_pred), tf.float32))
-
-
-def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
-    """Creates the training operation for a neural network in Tensorflow
-    using the Adam optimization algorithm.
-
-    Args:
-        loss: loss of network
-        alpha: learning rate
-        beta1: weight used for first moment
-        beta2: weight used for second moment
-        epsilon: small number to avoid division by 0
-
-    Returns:
-        Adam optimization operation
-    """
-    opt = tf.train.AdamOptimizer(alpha, beta1, beta2, epsilon)
-
-    return opt.minimize(loss)
-
-
-def create_batch_norm_layer(prev, n, activation):
-    """Creates a batch normalization layer for a neural network in Tensorflow
-
-    Args:
-        prev: activated output of previous layer
-        n: number of nodes in layer to be created
-        actiavtion: activation function that should be used for the output
-            of the layer
-
-    Returns:
-        Tensor of the activated output for the layer
-    """
-    # Create layer
-    kernel = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    base = tf.layers.Dense(n, kernel_initializer=kernel)
-
-    # Init variables
-    mean, variance = tf.nn.moments(base(prev), axes=[0])
-    gamma = tf.Variable(tf.ones([n]), trainable=True)
-    beta = tf.Variable(tf.zeros([n]), trainable=True)
-    epsilon = 1e-8
-
-    batch_norm = tf.nn.batch_normalization(
-        base(prev), mean, variance, beta, gamma, epsilon
-    )
-
-    return activation(batch_norm)
-
-
-def create_placeholders(nx, classes):
-    """
-    Creates placeholders x and y.
-    * @nx: number of feature columns in our data.
-    * @classes: number of classes in our classifier
-    Return: 2 placeholders
-    """
-    x = tf.placeholder(name='x', dtype=float, shape=[None, nx])
-    y = tf.placeholder(name='y', dtype=float, shape=[None, classes])
-    return x, y
-
-
-def create_layer(prev, n, activation):
-    """Creates a layer of neurons
-    Args:
-      prev: tensor output of the previous layer
-      n: number of nodes in the layer to create
-      activation: activation function that the layer should use
-    Returns:
-      tensor output of the layer
-    """
-    theta = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    return tf.layers.dense(prev, n, activation, kernel_initializer=theta)
-
-
-def forward_prop(prev, layers=[], activations=[]):
+def forward_prop(prev, layers=[], activations=[], epsilon=1e-8):
     """#all layers get batch_normalization but the last one,
     that stays without any activation or normalization"""
-    y_pred = prev
-    for i in range(len(layers)):
+    for i, n in enumerate(layers):
+        kernel = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+        dense = tf.layers.Dense(n, kernel_initializer=kernel, name='dense')
+        z = dense(prev)
         if i < len(layers) - 1:
-            y_pred = create_batch_norm_layer(y_pred, layers[i], activations[i])
+            mean, variance = tf.nn.moments(z, axes=[0])
+            gamma = tf.Variable(tf.ones([n]), trainable=True, name='gamma')
+            beta = tf.Variable(tf.zeros([n]), trainable=True, name='beta')
+            z_norm = tf.nn.batch_normalization(
+                z, mean, variance, beta, gamma, epsilon
+            )
+            prev = activations[i](z_norm)
         else:
-            y_pred = create_layer(y_pred, layers[i], activations[i])
+            prev = z
 
-    return y_pred
+    return prev
 
 
 def shuffle_data(X, Y):
@@ -147,41 +69,48 @@ def model(Data_train, Data_valid, layers, activations, alpha=0.001, beta1=0.9,
     Returns:
         Path to model
     """
-    # Get X_train, Y_train, X_valid, and Y_valid from Data_train and Data_valid
-    X_train, Y_train = Data_train[0], Data_train[1]
-    X_valid, Y_valid = Data_valid[0], Data_valid[1]
-    # Initialize x, y, y_pred, loss, accuracy
-    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-    y_pred = forward_prop(x, layers, activations)
-    loss = tf.losses.softmax_cross_entropy(y, y_pred)
-    accuracy = calculate_accuracy(y, y_pred)
-    # Intialize alpha and global_step variable (not trainable)
-    global_step = tf.Variable(0)
-    alpha = tf.train.inverse_time_decay(
-        alpha, global_step, 1, decay_rate, staircase=True
-        )
+    X_train, Y_train = Data_train
+    X_valid, Y_valid = Data_valid
 
-    train_op = create_Adam_op(loss, alpha, beta1, beta2, epsilon)
+    m, nx = X_train.shape
+    classes = Y_train.shape[1]
 
-    # Create collection
+    x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
+    y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
     tf.add_to_collection('x', x)
     tf.add_to_collection('y', y)
-    tf.add_to_collection('y_pred', y_pred)
-    tf.add_to_collection('loss', loss)
-    tf.add_to_collection('accuracy', accuracy)
-    tf.add_to_collection('train_op', train_op)
 
-    # Get batch info
-    m = X_train.shape[0]
-    batches = m / batch_size
-    if batches % 1 != 0:
-        batches = int(batches) + 1
-    else:
-        batches = int(batches)
+    y_pred = forward_prop(x, layers, activations, epsilon)
+    tf.add_to_collection('y_pred', y_pred)
+
+    loss = tf.losses.softmax_cross_entropy(y, y_pred)
+    tf.add_to_collection('loss', loss)
+
+    correct = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+    tf.add_to_collection('accuracy', accuracy)
+
+    global_step = tf.Variable(0, trainable=False)
+    decay_steps = m // batch_size
+    if m % batch_size != 0:
+        decay_steps += 1
+
+    alpha = tf.train.inverse_time_decay(
+        alpha, global_step, decay_steps, decay_rate, staircase=True
+        )
+    train_op = tf.train.AdamOptimizer(
+        alpha, beta1, beta2, epsilon
+    ).minimize(loss, global_step)
+    tf.add_to_collection('train_op', train_op)
 
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init)
+
+        m = X_train.shape[0]
+        batches = m // batch_size
+        if batches % 1 != 0:
+            batches += 1
 
         for i in range(epochs + 1):
             train_cost = loss.eval({x: X_train, y: Y_train})
