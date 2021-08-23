36726d3b7d34aade1344d17d0fd01bb80a6cdb85
Kyle <kyg555@outlook.com>
2021-08-22 17:26:56 -0700

    Add task 13

---
+++ supervised_learning/0x01-optimization/13-batch_norm.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Contains the function batch_norm()
+"""
+import numpy as np
+from numpy.core.fromnumeric import mean
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """Normalizes an unactivated output of a neural network using
+    batch normalization
+
+    Args:
+        Z: numpy.ndarray, shape: (m, n) - array to normalize
+            * m: number of data points
+            * n: number of features in Z
+        gamma: numpy.ndarray, shape (1, n) - contains scales used for
+            batch normalization
+        beta: numpy.ndarray, shape (1, n) - contains the offsets used
+            for batch normalization
+        epsilon: small number to avoid division by 0
+
+    Returns:
+        normalized Z matrix
+    """
+    variance, mean = np.var(Z, axis=0), np.mean(Z, axis=0)
+    Z_norm = (Z - mean) / (np.sqrt(variance + epsilon))
+
+    return gamma * Z_norm + beta
