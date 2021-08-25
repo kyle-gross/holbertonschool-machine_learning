f7354bf16c1ac68c04363ab6ae4f6d4655234307
Kyle <kyg555@outlook.com>
2021-08-25 11:40:32 -0700

    Add task 1

---
+++ supervised_learning/0x00-error_analysis/1-sensitivity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Contains the function sensitivity()
+"""
+import numpy as np
+
+
+def sensitivity(confusion):
+    """Calculates the sensitivity (recall) for each class in a
+    confusion matrix.
+
+    Args:
+        confusion: numpy.ndarray, shape(classes,classes) - confusion matrix
+
+    Returns:
+        numpy.ndarray, shape(classes,) - contains sensitivity of each class
+    """
+    FN = confusion.sum(axis=1) - np.diag(confusion)
+    TP =  np.diag(confusion)
+    TPR = TP / (TP + FN)
+
+    return TPR
