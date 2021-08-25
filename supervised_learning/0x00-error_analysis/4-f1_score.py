bbe81a3957344410718c1fdf256df55e899e535f
Kyle <kyg555@outlook.com>
2021-08-25 12:10:54 -0700

    add task 4

---
+++ supervised_learning/0x00-error_analysis/4-f1_score.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Contains the function f1_score()
+"""
+from typing import TYPE_CHECKING
+import numpy as np
+
+
+def f1_score(confusion):
+    """Calculates the F1 score of a confusion matrix
+
+    Args:
+        confusion: numpy.ndarray, shape(classes,classes) - confusion matrix
+
+    Returns:
+        numpy.ndarray, shape(classes,) - contains F1 score for each class
+    """
+    FP = confusion.sum(axis=0) - np.diag(confusion)
+    FN = confusion.sum(axis=1) - np.diag(confusion)
+    TP = np.diag(confusion)
+    F1 = (2 * TP) / (2 * TP + FP + FN)
+
+    return F1
