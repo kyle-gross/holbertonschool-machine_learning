6931514a95587f6045902cd7cb5ebc5c7170e3cb
Kyle <kyg555@outlook.com>
2021-10-21 15:31:35 -0700

    Add task 3

--- supervised_learning/0x01-face_verification/align.py
+++ supervised_learning/0x01-face_verification/align.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Contains the class FaceAlign"""
 
-import numpy as np
 import dlib
+import numpy as np
 
 
 class FaceAlign:
@@ -16,3 +16,20 @@ class FaceAlign:
         """
         self.detector = dlib.get_frontal_face_detector()
         self.shape_predictor = dlib.shape_predictor(shape_predictor_path)
+
+    def detect(self, image):
+        """Detects a face in an image
+
+        Args:
+            image (rank 3 ndarray): contains the image to detect in
+        
+        Returns:
+            dlib.rectangle containing the boundary box for the face
+            or None if failure
+        """
+        faces = self.detector(image, 1)
+
+        if len(faces) == 1:
+            return max(faces, key=lambda rect: rect.width() * rect.height())
+        else:
+            return None
