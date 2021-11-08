92d4f2ebed4b91c31220267cc137a47229a4c8e2
Kyle <kyg555@outlook.com>
2021-11-08 13:03:46 -0800

    Add task 5

---
+++ math/0x00-advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Contains the function definiteness()"""
+
+import numpy as np
+
+
+def definiteness(matrix):
+    """Calculates the definiteness of a matrix
+
+    Args:
+        matrix (ndarray)(n,n): calculate the definiteness of
+
+    Returns:
+        string representing definiteness of the matrix
+    """
+    if type(matrix) is not np.ndarray:
+        raise TypeError('matrix must be a numpy.ndarray')
+
+    if len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1]:
+        return None
+
+    eigvals = np.linalg.eigvals(matrix)
+
+    if np.all(eigvals > 0):
+        return "Positive definite"
+    elif np.all(eigvals >= 0):
+        return "Positive semi-definite"
+    elif np.all(eigvals < 0):
+        return "Negative definite"
+    elif np.all(eigvals <= 0):
+        return "Negative semi-definite"
+    else:
+        return "Indefinite"    
