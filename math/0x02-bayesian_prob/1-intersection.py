9678da9001e89804b2d5d9f5c0a25501a8a84752
Kyle Gross <kyg555@outlook.com>
2021-11-15 14:59:51 -0800

    Remove extra space

--- math/0x02-bayesian_prob/1-intersection.py
+++ math/0x02-bayesian_prob/1-intersection.py
@@ -67,7 +67,7 @@ def intersection(x, n, P, Pr):
         raise TypeError('P must be a 1D numpy.ndarray')
     if type(Pr) is not np.ndarray or Pr.shape != P.shape:
         raise TypeError(
-            'Pr must be a  numpy.ndarray with the same shape as P'
+            'Pr must be a numpy.ndarray with the same shape as P'
         )
     if not (np.all(P >= 0) and np.all(P <= 1)):
         raise ValueError('All values in P must be in the range [0, 1]')
