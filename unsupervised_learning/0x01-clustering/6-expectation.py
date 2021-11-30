cf972228e9bf18f30238ba35aa77ad3ca45fec71
Kyle Gross <kyg555@outlook.com>
2021-11-30 12:51:21 -0800

    Try different checking style

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -23,15 +23,18 @@ def expectation(X, pi, m, S):
     if (type(X) is not np.ndarray or X.ndim != 2 or
        type(pi) is not np.ndarray or pi.ndim != 1 or
        type(m) is not np.ndarray or m.ndim != 2 or
-       type(S) is not np.ndarray or S.ndim != 3 or
-       S.shape[0] != pi.size or not np.isclose(np.sum(pi), [1])[0] or
-       S.shape[1] != S.shape[2] or S.shape[1] != X.shape[1] or
-       m.shape[1] != X.shape[1]):
+       type(S) is not np.ndarray or S.ndim != 3):  # or
+    #    S.shape[0] != pi.size or not np.isclose(np.sum(pi), [1])[0] or
+    #    S.shape[1] != S.shape[2] or S.shape[1] != X.shape[1] or
+    #    m.shape[1] != X.shape[1]):
         return None, None
 
     k = pi.shape[0]
 
     pdfs = [pdf(X, m[i], S[i]) * pi[i] for i in range(k)]
+
+    if None in pdfs:
+        return None, None
 
     g = np.array(pdfs)  # Probabilities
     likelihood = g.sum(axis=0)
