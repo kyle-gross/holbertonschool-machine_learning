5adc5aeffeefe78d31fddd0dcce4cc26f6af259a
Kyle Gross <kyg555@outlook.com>
2021-12-03 16:50:50 -0800

    Add task 1

---
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Contains the function regular()"""
+
+import numpy as np
+
+
+def regular(P):
+    """Determines the steady state probabilities of a regular markov chain
+
+    Args:
+        P (ndarray)(n,n): transition matrix
+            P[i,j]: probability of transitioning from state i to state j
+            n: no. states in markov chain
+    
+    Returns:
+        steady // None if failure
+        steady (ndarray)(1,n): steady state probabilities
+    """
+    if (type(P) is not np.ndarray or P.ndim != 2 or
+       P.shape[0] != P.shape[1] or not (P > 0).all()):
+        return None
+
+    evals, evecs = np.linalg.eig(P.T)
+    evec1 = evecs[:, np.isclose(evals, 1)]
+    evec1 = evec1[:, 0]
+    steady = evec1 / evec1.sum()
+
+    return steady
