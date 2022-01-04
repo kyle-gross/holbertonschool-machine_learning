889e550708ec240d7c0c4181768773aab6b2ebcd
Kyle Gross <kyg555@outlook.com>
2022-01-04 14:48:30 -0800

    Debug task 0

--- unsupervised_learning/0x00-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x00-autoencoders/0-vanilla.py
@@ -19,27 +19,30 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
         decoder: decoder model
         auto: full autoencoder model
     """
-    x = keras.Input(shape=(input_dims,))
+    x = keras.layers.Input(shape=(input_dims,))
+    x_encoded = keras.Input(shape=(latent_dims,))
+    x_decoded = x_encoded
+
     encoded = keras.layers.Dense(hidden_layers[0], activation='relu')(x)
-    latent = keras.layers.Input(shape=(latent_dims,))
-    x_decoded = latent
 
     for i in range(1, len(hidden_layers)):
         encoded = keras.layers.Dense(
             hidden_layers[i], activation='relu'
         )(encoded)
+    encoded = keras.layers.Dense(latent_dims, activation='relu')(encoded)
     
-    # encoded = keras.layers.Dense(latent_dims, activation='relu')(encoded)
-    decoded = keras.layers.Dense(latent_dims, activation='relu')(x_decoded)
+    decoded = keras.layers.Dense(
+        hidden_layers[-1], activation='relu'
+    )(x_decoded)
 
     for i in range(len(hidden_layers) - 1, -1, -1):
-        if i == len(hidden_layers) - 1:
-            decoded = keras.layers.Dense(i, activation='sigmoid')(decoded)
-        else:
-            decoded = keras.layers.Dense(i, activation='sigmoid')(decoded)
-    
-    encoder = keras.Model(x, encoded)
-    decoder = keras.Model(latent, decoded)
+        decoded = keras.layers.Dense(
+            hidden_layers[i], activation='relu'
+        )(decoded)
+    decoded = keras.layers.Dense(input_dims, activation='sigmoid')(decoded)
+
+    encoder = keras.models.Model(x, encoded)
+    decoder = keras.models.Model(x_encoded, decoded)
     auto = keras.Model(x, decoder(encoder(x)))
 
     auto.compile(optimizer='adam', loss='binary_crossentropy')
