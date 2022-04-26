04d64849e0d5d0db9e7990bc78b812c582773c29
Kyle Gross <kyg555@outlook.com>
2022-04-25 18:36:46 -0700

    Remove excess

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -7,21 +7,20 @@ import time
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        user = sys.argv[1]
-        r = requests.get(user)
+    if len(sys.argv) < 2:
+        exit()
+    user = sys.argv[1]
+    r = requests.get(user)
 
-        if r.status_code == 404:
+    if r.status_code == 404:
+        print('Not found')
+    if r.status_code == 403:
+        reset_time = int(r.headers['X-Ratelimit-Reset']) - time.time()
+        minutes = round(reset_time / 60)
+        print('Reset in {} min'.format(minutes))
+    if r.status_code == 200:
+        location = r.json()['location']
+        if location is not None:
+            print('{}'.format(location))
+        else:
             print('Not found')
-        if r.status_code == 403:
-            reset_time = int(r.headers['X-Ratelimit-Reset']) - time.time()
-            minutes = round(reset_time / 60)
-            print('Reset in {} min'.format(minutes))
-        if r.status_code == 200:
-            location = r.json()['location']
-            if location is not None:
-                print('{}'.format(location))
-            else:
-                print('Not found')
-    else:
-        print('Invalid url. Run the script again with a user\'s url.')
