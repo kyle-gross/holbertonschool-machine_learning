2aa4f0d5fff6d51a3eb116e7b15359ede0543681
Kyle Gross <kyg555@outlook.com>
2022-04-26 15:38:22 -0700

    Add task 4

---
+++ pipeline/0x01-apis/4-rocket_frequency.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""This script displays the number of launches per rocket from the SpaceX API
+"""
+
+import requests
+
+
+if __name__ == '__main__':
+    url = 'https://api.spacexdata.com/v4/launches'
+    rocket_url = 'https://api.spacexdata.com/v4/rockets/'
+    launches = requests.get(url).json()
+    rocket_launches = {}
+
+    for launch in launches:
+        rocket_id = launch['rocket']
+        rocket = requests.get(rocket_url + rocket_id).json()
+        name = rocket['name']
+
+        if name not in rocket_launches.keys():
+            rocket_launches[name] = 1
+        else:
+            rocket_launches[name] += 1
+
+    rocket_launches = sorted(rocket_launches.items(), key=lambda x: x[1])
+    
+    for k, v in reversed(rocket_launches):
+        print('{}: {}'.format(k, v))
