780904d85c3875495b27372c51ceadcd5a4ef5f2
Kyle Gross <kyg555@outlook.com>
2022-05-12 18:35:52 -0700

    Add delimiter

--- pipeline/0x00-databases/19-bonus.sql
+++ pipeline/0x00-databases/19-bonus.sql
@@ -1,4 +1,5 @@
 -- Creates a stored procedure AddBonus that adds a new correction for a student
+DELIMITER //
     CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
     BEGIN
         IF NOT EXISTS (SELECT name FROM projects WHERE name=project_name) THEN
@@ -8,4 +9,5 @@
         SET @project_id = (SELECT id FROM projects WHERE name = project_name);
 
         INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, @project_id, score);
-    END;
+    END //
+DELIMITER ;
