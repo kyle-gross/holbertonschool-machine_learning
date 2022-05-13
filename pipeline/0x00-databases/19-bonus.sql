b8e21565e28d5105542a1924887b0c392280ae20
Kyle Gross <kyg555@outlook.com>
2022-05-12 18:29:03 -0700

    Take 2

--- pipeline/0x00-databases/19-bonus.sql
+++ pipeline/0x00-databases/19-bonus.sql
@@ -1,12 +1,11 @@
 -- Creates a stored procedure AddBonus that adds a new correction for a student
-CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
-BEGIN
-    IF NOT EXISTS (SELECT name FROM projects WHERE name=project_name) THEN
-        INSERT INTO projects(name)
-        VALUES (project_name);
-    END IF;
+    CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
+    BEGIN
+        IF NOT EXISTS (SELECT name FROM projects WHERE name=project_name) THEN
+            INSERT INTO projects(name) VALUE (project_name);
+        END IF;
 
-    SET @project_id = (SELECT id FROM projects WHERE projects.name = project_name);
+        SET @project_id = (SELECT id FROM projects WHERE name = project_name);
 
-    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, @project_id, score);
-END;
+        INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, @project_id, score);
+    END;
