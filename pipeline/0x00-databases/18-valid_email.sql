baabe3f3ed34a715f234154a6e92a05e957a8191
Kyle Gross <kyg555@outlook.com>
2022-05-13 17:07:16 -0700

    Add newline

--- pipeline/0x00-databases/18-valid_email.sql
+++ pipeline/0x00-databases/18-valid_email.sql
@@ -4,9 +4,7 @@ CREATE TRIGGER change_email
     BEFORE UPDATE
     ON users
     FOR EACH ROW
-    BEGIN
         IF STRCMP(old.email, new.email) <> 0 THEN
             SET new.valid_email = 0
         END IF;
-    END //
 DELIMITER ;
