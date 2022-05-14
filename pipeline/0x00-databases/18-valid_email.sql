e3006c3425c01de347b7768f2cfc16e30170c31c
Kyle Gross <kyg555@outlook.com>
2022-05-13 17:01:10 -0700

    Add delimiter

--- pipeline/0x00-databases/18-valid_email.sql
+++ pipeline/0x00-databases/18-valid_email.sql
@@ -1,8 +1,11 @@
 -- Creates a trigger that resets the attribute valid_email only when email has been changed
+DELIMITER //
 CREATE TRIGGER change_email
-BEFORE UPDATE
-ON users
-FOR EACH ROW
-    IF (old.email <> new.email) THEN
-        SET new.valid_email = 0
-    END IF;
+    BEFORE UPDATE
+    ON users
+    FOR EACH ROW
+        IF (old.email <> new.email) THEN
+            SET new.valid_email = 0
+        END IF;
+    END;
+DELIMITER ;
